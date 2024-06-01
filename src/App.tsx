4a4e8b5d77f950cb8742bbfe79a7c4b86336924b
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2024-06-01 16:28:34 -0300

    feat: integrate Tailwind CSS and update project configurations
    - Installed Tailwind CSS along with its dependencies: `tailwindcss`, `postcss`, and `autoprefixer`.
    - Configured Tailwind CSS by updating `tailwind.config.js` to scan content from `./index.html` and `./src/**/*.{js,ts,jsx,tsx}`.
    - Added Tailwind CSS directives to `src/index.css` and imported it in the main entry file.
    - Updated `App.tsx` component to utilize Tailwind CSS classes for styling, replacing custom CSS classes.
    - Enhanced `package.json` by adding new dependencies: `shadcdn` and `taildincss`.
    - Updated various configurations in `tsconfig.json` and `vite.config.ts` for better path resolution and aliasing.
    - Added `paths` configuration to `tsconfig.json` for cleaner import statements.
    - Improved `vite.config.ts` with path aliasing to streamline imports.

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,34 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './globals.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className="flex justify-center space-x-4">
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="w-20" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="w-20" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <h1 className="text-3xl font-bold text-center mt-4">Vite + React</h1>
+      <div className="card p-4 bg-white shadow-md rounded-lg text-center mt-4">
+        <button
+          className="font-bold py-2 px-4 rounded-xl border-2 border-blue-300 hover:border-blue-700 transition-colors"
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
         </button>
-        <p>
+        <p className="mt-4">
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="read-the-docs text-center mt-4">
         Click on the Vite and React logos to learn more
       </p>
     </>
