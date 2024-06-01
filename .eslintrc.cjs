65c0c7e404e79e1a9e6c7cfb88ab0b3522a5de0a
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2024-06-01 16:35:54 -0300

    chore(deps): add new ESLint plugins for code quality and style enforcement
    - Added the following ESLint plugins:
      - `eslint-config-prettier` for disabling ESLint rules that might conflict with Prettier.
      - `eslint-plugin-mui-path-imports` for enforcing correct import paths in MUI.
      - `eslint-plugin-prettier` for integrating Prettier as an ESLint rule.
      - `eslint-plugin-simple-import-sort` for sorting import statements.
      - `eslint-plugin-tailwindcss` for enforcing Tailwind CSS class naming conventions.
      - `eslint-plugin-unicorn` for various code quality rules.
      - `eslint-plugin-unused-imports` for detecting and removing unused imports.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,79 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs','src/components/ui'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'mui-path-imports', 'unicorn'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'mui-path-imports/mui-path-imports': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        endOfLine: 'auto',
+      },
+    ],
+    'unicorn/filename-case': [
+      'error',
+      {
+        case: 'kebabCase',
+        ignore: ['/dist', 'App*'],
+      },
+    ],
   },
+  overrides: [    {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js'],
+    plugins: [
+      '@typescript-eslint',
+      'unused-imports',
+      'tailwindcss',
+      'simple-import-sort',
+    ],
+    extends: [
+      'plugin:tailwindcss/recommended',
+      'plugin:prettier/recommended',
+    ],
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          endOfLine: 'auto',
+        },
+      ],
+      'max-params': ['error', 3], // Limit the number of parameters in a function to use object instead
+      'react/destructuring-assignment': 'off', // Vscode doesn't support automatically destructuring, it's a pain to add a new variable
+      'react/require-default-props': 'off', // Allow non-defined react props as undefined
+      '@typescript-eslint/comma-dangle': 'off', // Avoid conflict rule between Eslint and Prettier
+      '@typescript-eslint/consistent-type-imports': 'error', // Ensure `import type` is used when it's necessary
+      'import/prefer-default-export': 'off', // Named export is easier to refactor automatically
+      'react-refresh/only-export-components': 'off', // Allow to export multiple components in a single file
+      'tailwindcss/classnames-order': [
+        'warn',
+        {
+          officialSorting: true,
+        },
+      ], // Follow the same ordering as the official plugin `prettier-plugin-tailwindcss`
+      'simple-import-sort/imports': 'error', // Import configuration for `eslint-plugin-simple-import-sort`
+      'simple-import-sort/exports': 'error', // Export configuration for `eslint-plugin-simple-import-sort`
+      '@typescript-eslint/no-unused-vars': 'off',
+      'tailwindcss/no-custom-classname': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'max-lines-per-function': 'off',
+      'unused-imports/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },],
 }
